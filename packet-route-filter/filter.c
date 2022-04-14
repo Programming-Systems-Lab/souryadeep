e3fa83f5e79117ea81c023ddea5ef74f6ea8bca4
Souryadeep <souryadeepsen80@gmail.com>
2022-04-14 11:21:07 -0400

    added comments to trace structs in bootlin more easily

--- packet-route-filter/filter.c
+++ packet-route-filter/filter.c
@@ -5,15 +5,22 @@
 #include <linux/in.h>
 #include <linux/udp.h>
 
+/*xdp_md defined in /include/uapi/linux/bpf.h*/
 int udpfilter(struct xdp_md *ctx) {
   bpf_trace_printk("got a packet\n");
   void *data = (void *)(long)ctx->data;
   void *data_end = (void *)(long)ctx->data_end;
+
+  /*ethhdr defined in: /include/uapi/linux/if_ether.h*/
   struct ethhdr *eth = data;
   if ((void*)eth + sizeof(*eth) <= data_end) {
+
+    /*iphdr: defined in /include/uapi/linux/ip.h*/  
     struct iphdr *ip = data + sizeof(*eth);
     if ((void*)ip + sizeof(*ip) <= data_end) {
       if (ip->protocol == IPPROTO_UDP) {
+
+	/*udphdr: defined in /include/uapi/linux/udp.h*/
         struct udphdr *udp = (void*)ip + sizeof(*ip);
         if ((void*)udp + sizeof(*udp) <= data_end) {
           if (udp->dest == ntohs(7999)) {
